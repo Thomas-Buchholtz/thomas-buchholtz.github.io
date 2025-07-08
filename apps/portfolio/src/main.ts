9dc9d2f82ebd5b9fd96d4e917c1f543f40e8ec3e
tommy <thomas.buchholtz@codecamp-n.com>
2025-07-08 16:52:51 +0200

    :sparkles: Added favicon and slightly improvements

--- apps/portfolio/src/main.ts
+++ apps/portfolio/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
+import 'bootstrap';
 
 bootstrapApplication(AppComponent, {
   providers: [
--- shared/src/lib/ui-components/navbar/navbar.ts
+++ shared/src/lib/ui-components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'lib-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
