5027c661c476caf6457583acccb4e213d1b59b0d
tommy <thomas.buchholtz@codecamp-n.com>
2025-07-24 22:23:38 +0200

    :sparkles: Added contact link and smooth scroll

--- shared/src/lib/ui-components/navbar/navbar.ts
+++ shared/src/lib/ui-components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener} from '@angular/core';
+import {AfterViewInit, Component, HostListener} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink} from "@angular/router";
 
@@ -8,11 +8,30 @@ import {RouterLink} from "@angular/router";
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+
+  ngAfterViewInit() {
+    this.scrollToContactIfRequested();
+  }
   isScrolled = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.pageYOffset > 10;
+  }
+
+  scrollToContact(): void {
+    const el = document.getElementById('contact');
+    if (el) {
+      el.scrollIntoView({behavior: 'smooth', block: 'start'});
+    } else {
+      console.warn('Kontakt-Element nicht gefunden');
+    }
+  }
+
+  private scrollToContactIfRequested(): void {
+    if (window.location.hash === '#kontakt') {
+      setTimeout(() => this.scrollToContact(), 100);
+    }
   }
 }
